bed98d55b6e7f96f202d79c1ef094c63a213fcc7
huangjiahua <hah@whu.edu.cn>
2019-09-29 12:34:25 +0800

    improve mrsw read

--- mrsw_ptr.h
+++ mrsw_ptr.h
@@ -133,23 +133,27 @@ private:
         return data | meta;
     }
 
-    static bool IsFree(uint64_t meta) {
+    static inline bool IsFree(uint64_t meta) {
         return (meta & 0xC000ull) == 0;
     }
 
-    static bool IsBeingWritten(uint64_t meta) {
+    static inline bool IsBeingWritten(uint64_t meta) {
         return (meta & 0x8000ull) != 0;
     }
 
-    static bool IsBeingRead(uint64_t meta) {
+    static inline bool IsWritable(uint64_t meta) {
+        return meta == 0;
+    }
+
+    static inline bool IsBeingRead(uint64_t meta) {
         return (meta & 0x4000ull) != 0;
     }
 
-    static uint64_t GetMeta(uint64_t value) {
+    static inline uint64_t GetMeta(uint64_t value) {
         return (value & 0xffffull);
     }
 
-    static uint64_t GetCounter(uint64_t value) {
+    static inline uint64_t GetCounter(uint64_t value) {
         return (value & 0x3fffull);
     }
 
@@ -159,7 +163,7 @@ private:
         again:
         uint64_t meta = GetMeta(value);
 
-        if (IsFree(meta)) {
+        if (IsWritable(meta)) {
             // good, no one is touching this, I'm gonna start write mode by setting the write bit
             uint64_t desired = value | 0x8000ull;
             bool ok = data_.compare_exchange_strong(value, desired);
@@ -194,49 +198,18 @@ private:
     }
 
     void PrepareRead() {
-        start:
         uint64_t value = data_.fetch_add(1);
-        value += 1;
         again:
         uint64_t meta = GetMeta(value);
-        uint64_t counter = GetCounter(value);
 
-        if (IsBeingRead(meta)) {
-            // good, other threads(or one thread) are reading, just join them(the counter represents how many
-            // threads are currently reading)
-            return;
-        } else if (IsBeingWritten(meta)) {
-            // bad, one thread is writing, try again and do busy waiting(the counter represents how many
-            // threads are waiting for the writer to end)
+        if (IsBeingWritten(meta)) {
             value = data_.load();
             goto again;
-        } else {
-            // well, it seems that this thread should start read mode(by setting the read bit, and the counter
-            // represents how many threads are trying to initiate read mode. Only one can do this by getting true
-            // in the CAS and others will get false and try again)
-            uint64_t desired = value | 0x4000ull;
-            bool ok = data_.compare_exchange_strong(value, desired);
-            if (!ok) {
-                // no! the value has been changed by others, let's look how it changes(the 'value' has been
-                // changed by the CAS)
-                goto again;
-            }
         }
     }
 
     void FinishRead() {
-        uint64_t value = data_.fetch_sub(1);
-        value -= 1;
-        again:
-        uint64_t counter = GetCounter(value);
-        if (counter == 0) {
-            // it seems that I'm the last reader, let's try to end the read mode(by clearing the read bit)
-            uint64_t desired = value & (~0x4000ull);
-            data_.compare_exchange_strong(value, desired);
-            // oops, at least one thread has changed this value. there is no need to try again, because the only
-            // possible change is someone start reading it and increase the counter, so I can say I'm not the last
-            // reader and it is not my responsibility to stop the read mode
-        }
+        data_.fetch_sub(1);
     }
 
     T *Get() {
